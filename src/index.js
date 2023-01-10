a874941cbb0f6fb7286e29eca3ebbe82dc7daefa
polyster20 <polyster20@gmail.com>
2023-01-10 23:52:23 +0100

    custom palette

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+
 import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import PageLogin from './pages/pageLogin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const darkDynamXTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#e6750c',
+      light: '#ffa545',
+      dark: '#ad4700',
+    },
+  },
+});
+
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<PageLogin />}>
 
@@ -13,6 +32,8 @@ const router = createBrowserRouter(createRoutesFromElements(
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={darkDynamXTheme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
