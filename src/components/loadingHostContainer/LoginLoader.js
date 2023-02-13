4b1c261dda639e5c8a7dc84d4c9ae90150147ce2
polyster20 <polyster20@gmail.com>
2023-02-13 12:06:37 +0100

    Loading screen becomes undraggable

--- src/components/loadingHostContainer/LoginLoader.js
+++ src/components/loadingHostContainer/LoginLoader.js
@@ -11,7 +11,7 @@ export default function LoginLoader(props) {
 	}
 
 	return (
-		<div style={loadingContainer}>
+		<div className="undraggable" style={loadingContainer}>
 			<CircularProgress />
 			<p> Connecting to {props.address}</p>
 		</div>
