539525ff924854ed694cf590511281342c68b46d
polyster20 <polyster20@gmail.com>
2023-02-13 15:11:50 +0100

    pageHome added

---
+++ src/pages/pageHome.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import PageComponent from '../components/page/page';
+
+export default function PageHome() {
+    console.log("home")
+    return (
+        <PageComponent>
+            <div>Home</div>
+        </PageComponent>
+    );
+}
