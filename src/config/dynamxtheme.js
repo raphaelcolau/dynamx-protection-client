1b0118a666339086db5373893135e9b576b5189d
polyster20 <polyster20@gmail.com>
2023-02-14 21:08:55 +0100

    createTheme refactor

---
+++ src/config/dynamxtheme.js
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material/styles';
+
+export const DynamXTheme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#e6750c',
+        light: '#ffa545',
+        dark: '#ad4700',
+      },
+      background: {
+        default: '#2F2F2F',
+        paper: '#2F2F2F',
+      },
+    },
+});
--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
+import { DynamXTheme } from './config/dynamxtheme';
 import PageLogin from './pages/pageLogin';
 import PageHome from './pages/pageHome';
 import PageAdd from './pages/pageAdd';
@@ -17,21 +18,6 @@ import PageWhitelist from './pages/pageWhitelist';
 import PageBlacklist from './pages/pageBlacklist';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const darkDynamXTheme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#e6750c',
-      light: '#ffa545',
-      dark: '#ad4700',
-    },
-    background: {
-      default: '#2F2F2F',
-      paper: '#2F2F2F',
-    },
-  },
-});
 
 const router = createBrowserRouter([
   { path: '/', element: <PageLogin /> },
@@ -44,7 +30,7 @@ const router = createBrowserRouter([
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkDynamXTheme}>
+    <ThemeProvider theme={DynamXTheme}>
       <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
