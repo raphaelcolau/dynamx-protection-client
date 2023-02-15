050946eec5a4c6bdc59007fe9928bc7b23f014a0
polyster20 <polyster20@gmail.com>
2023-02-15 17:02:35 +0100

    fix of urls starting with //

--- src/utils/files.js
+++ src/utils/files.js
@@ -2,7 +2,13 @@ const { ipcMain, shell } = require("electron")
 
 exports.browserUrl = () => {
     ipcMain.on("browser-url", (event, arg) => {
-        shell.openExternal(arg)
+        const url = arg;
+        console.log(typeof url);
+        if (url.startsWith("//")) {
+            shell.openExternal(`http:${url}`)
+        } else {
+            shell.openExternal(url)
+        }
         return { action: 'deny' };
     })
 }
